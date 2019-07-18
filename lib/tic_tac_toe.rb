5f78280698bd9b6c5b27b420c660466ec9b6a963
Hillary Spitzer <hspitzer2@gmail.com>
2019-07-18 18:27:14 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,34 @@
+class TicTacToe
+
+
+  def initialize(board = nil)
+     @board = Array.new(9, " ")
+
+   end
+
+
+    WIN_COMBINATIONS = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [0,4,8],
+      [6,4,2],
+      [2,5,8]
+     ]
+     def board
+       puts " #{board[0]} | #{board[1]} | #{board[2]} "
+       puts "-----------"
+       puts " #{board[3]} | #{board[4]} | #{board[5]} "
+       puts "-----------"
+       puts " #{board[6]} | #{board[7]} | #{board[8]} "
+     end
+
+     def display_board
+       @board = WIN_COMBINATIONS
+     end
+
+
+
+end
